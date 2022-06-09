06afcad746c247f83a22c38658452ccd791ab0c0
Usborn Ocampo <usborn116@gmail.com>
2022-06-09 08:16:33 -0700

    refactor code to use the .times method

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,17 +1,11 @@
 def bubble_sort(array)
-    unsorted = true
-    count = array.length - 1
-    while unsorted do
+    (array.length - 1).times do
         for i in 0...(array.length - 1)
             if array[i] > array[i+1]
                 array[i], array[i+1] = array[i+1], array[i]
             end
         end
-        count -= 1
-        if count == 0
-            unsorted = false
-        end
     end
-    array
+    p array
 end
 bubble_sort([4,3,78,2,0,2])
