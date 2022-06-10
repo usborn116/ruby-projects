0cbc2165a41ab4cc91fb99867bbeb8f0836c832b
Usborn Ocampo <usborn116@gmail.com>
2022-06-10 13:10:51 -0700

    refactored per RubyCop

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,11 +1,11 @@
+# frozen_string_literal: true
+
 def bubble_sort(array)
-    (array.length - 1).times do
-        for i in 0...(array.length - 1)
-            if array[i] > array[i+1]
-                array[i], array[i+1] = array[i+1], array[i]
-            end
-        end
+  (array.length - 1).times do
+    (0...(array.length - 1)).each do |i|
+      array[i] > array[i + 1] ? (array[i], array[i + 1] = array[i + 1], array[i]) : next
     end
-    p array
+  end
+  p array
 end
-bubble_sort([4,3,78,2,0,2])
+bubble_sort([4, 3, 78, 2, 0, 2])
